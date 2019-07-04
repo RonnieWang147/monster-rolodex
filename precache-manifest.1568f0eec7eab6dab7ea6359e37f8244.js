1686b4529c790095af669beaaf0f6bac2bd2c761
Yue Wang <yuewang@Lulus-MBP.home>
2019-07-04 15:20:05 -0400

    Updates

---
+++ precache-manifest.1568f0eec7eab6dab7ea6359e37f8244.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b8c74a8322d40b521fb2ef812b88551",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2498efbc009d0e12b875",
+    "url": "/monster-rolodex/static/css/main.9160c666.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2498efbc009d0e12b875",
+    "url": "/monster-rolodex/static/js/main.abd93622.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
